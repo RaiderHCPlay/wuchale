6ace6076b1ce0defc4d2de67c2009d076295eec2
RaiderHCPlay <fabryckipaweldev@gmail.com>
2026-07-14 21:43:44 +0200

    feat: write content to gitignore

---
+++ packages/wuchale-add/src/gitignore.ts
@@ -0,0 +1,10 @@
+import { existsSync, readFileSync, writeFileSync } from 'fs'
+
+export function writeGitignore() {
+    if (existsSync('.gitignore')) {
+        let content = readFileSync('.gitignore', 'utf8')
+        if (content.includes('.wuchale')) return
+        content += '\n#Wuchale\n.wuchale\n'
+        writeFileSync('.gitignore', content, 'utf8')
+    }
+}
